c8230ae2081e7ec9de31b5050ba5f1bb11092e26
RoBra18 <brand8n123@gmail.com>
2026-05-21 17:07:41 -0400

    Phase1: Module2 and Module 3

--- smart-recipe-box/src/app/app.ts
+++ smart-recipe-box/src/app/app.ts
@@ -5,11 +5,21 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
-    <h1>Smart Recipe Box</h1>
+    <h1>{{ title() }}</h1>
+    <button (click)="clickLogButton('Button 1 Clicked')" >Button 1</button>
+    <button (click)="clickLogButton('Button 2 Clicked')" >Button 2</button>
     <router-outlet />
+
   `,
   styles: [],
 })
 export class App {
-  protected readonly title = signal('smart-recipe-box');
+  protected readonly title = signal('My Recipe Box');
+
+  /*protected clickLogButton = (text: string) =>{
+    console.log(text);
+  }*/
+  protected clickLogButton (text: string) {
+    console.log(text);
+  } //It is prefered this notation
 }
