b36e7aec5603455508c88636cc90e34c69d14630
RoBra18 <brand8n123@gmail.com>
2026-05-24 22:02:23 -0400

    Phase2: Module 4

--- smart-recipe-box/src/app/app.ts
+++ smart-recipe-box/src/app/app.ts
@@ -1,13 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {RecipeModel} from "./models";
+import {MOCK_RECIPES} from "./mock-recipes";
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   template: `
     <h1>{{ title() }}</h1>
-    <button (click)="clickLogButton('Button 1 Clicked')" >Button 1</button>
-    <button (click)="clickLogButton('Button 2 Clicked')" >Button 2</button>
+    <button (click)="switchRecipeShown('back')" >Back</button>
+    <button (click)="switchRecipeShown('forward')" >Forward</button>
+    <div>
+      <p>Name: {{recipe().name}}</p>
+      <p>Description: {{recipe().description}}</p>
+
+    </div>
     <router-outlet />
 
   `,
@@ -22,4 +28,19 @@ export class App {
   protected clickLogButton (text: string) {
     console.log(text);
   } //It is prefered this notation
+
+  //Module 4
+  index: number = 0;
+  recipe = signal<RecipeModel>(MOCK_RECIPES[this.index]);
+  
+  protected switchRecipeShown (text:"forward" | "back") {
+    if(text === "forward"){
+      this.index = this.index + 1 >= MOCK_RECIPES.length ? 0 : this.index + 1;
+    }
+    else{
+      this.index = this.index - 1 <= -1 ? MOCK_RECIPES.length -1 : this.index - 1;
+    }
+    //console.log(this.index);
+    this.recipe.set(MOCK_RECIPES[this.index]);
+  }
 }
